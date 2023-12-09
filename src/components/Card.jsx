65cbb75de1d1ffb984c993a73e86bade1b960537
LazY <darshitmistry2@gmail.com>
2023-12-09 15:34:58 +0530

    feat:motion components added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
+import {motion} from 'framer-motion'
 
-const Card = ({ data }) => {
+const Card = ({ data ,refrence}) => {
   return (
-    <div className="relative flex-shrink-0 w-60 h-72 rounded-[40px]  bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+    <motion.div drag dragConstraints={
+refrence
+    } className="relative flex-shrink-0 w-60 h-72 rounded-[40px]  bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
       <FaRegFileAlt />
       <p className="text-sm leading-tight mt-5 font-semibold">{data.desc}</p>
       <div className="footer absolute bottom-0 w-full  left-0">
@@ -29,7 +32,7 @@ const Card = ({ data }) => {
           </div>
         ) : null}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Forground.tsx
+++ src/components/Forground.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React,{useRef} from "react";
 import Card from "./Card";
 
 const Forground = () => {
+    const ref = useRef(null)
   const data = [
     {
       desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
@@ -35,11 +36,12 @@ const Forground = () => {
       },
   ];
   return (
-    <div className="fixed top-0 w-full left-0 z-[3] h-full gap-10 flex flex-wrap p-5">
+    <div ref={ref} className="fixed top-0 w-full left-0 z-[3] h-full gap-10 flex flex-wrap p-5">
       {data.map((item, i) => {
         return (
           <Card
            data={item}
+           refrence ={ref}
           />
         );
       })}
